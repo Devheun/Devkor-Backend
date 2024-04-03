105215b22456be3e364e14881d491607290cdf82
DevHeun <dudwo5600@korea.ac.kr>
2024-04-03 13:04:28 +0900

    mod: CI/CD

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!!! + 배포 테스트';
+    return 'Hello World!!! + CI/CD Test';
   }
 }
