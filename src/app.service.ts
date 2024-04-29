a3c0963ef983d684742da9b6bca10a722588f4a2
Devheun <dudwo5600@korea.ac.kr>
2024-04-29 10:49:38 +0900

    mod:: CI/CD Test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World~~~ + CI/CD Test!!!';
+    return 'New laptop CI/CD Test!!!';
   }
 }
